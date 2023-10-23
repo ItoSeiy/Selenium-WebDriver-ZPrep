ccd7868cf9ba42c0d2364de99f945da40ebce8e4
ItoSeiya <itoseiya1219@gmail.com>
2023-10-23 23:47:38 +0900

    style : pathのコメント

--- zprep/libs/path.py
+++ zprep/libs/path.py
@@ -12,12 +12,10 @@ def get_assets_path(relative_path: str) -> str:
     """
     # path.py ファイルのディレクトリを取得
     path_directory = os.path.dirname(__file__)
-    # assetsディレクトリの相対パス
-    assets_relative_path = os.path.join("..", "..", "assets")
-    # assetsディレクトリの絶対パスを生成
-    assets_path = os.path.normpath(os.path.join(path_directory, assets_relative_path))
-    # assetsディレクトリの相対パス
-    relative_path = os.path.join(assets_relative_path, relative_path)
+    # assetsディレクトリへの相対パス
+    to_assets_relative_path = os.path.join("..", "..", "assets")
+    # 指定されたassetsディレクトリの相対パス
+    relative_path = os.path.join(to_assets_relative_path, relative_path)
     # assetsディレクトリの絶対パスを生成
     path = os.path.normpath(os.path.join(path_directory, relative_path))
     return path
