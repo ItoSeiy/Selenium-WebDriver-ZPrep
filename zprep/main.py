13416477b22bf29bb4ced07a8ac2e16f4156bd10
ItoSeiya <itoseiya1219@gmail.com>
2023-10-23 23:25:37 +0900

    fix : main import

--- zprep/main.py
+++ zprep/main.py
@@ -1,21 +1,21 @@
-import libs.const
-import libs.gui
-import libs.save
+import libs.const as const
+import libs.gui as gui
+import libs.save as save
 
 
-def _on_start_button_click(save_data: libs.save):
+def _on_start_button_click(save_data: save):
     if save_data is not None:
-        libs.save.SaveData.save_to_json(
-            save_data, libs.const.Save.Path.DATA_PATH, libs.const.Save.Path.FILE_NAME
+        save.SaveData.save_to_json(
+            save_data, const.Save.Path.DATA_PATH, const.Save.Path.FILE_NAME
         )
     print(save_data)
 
 
 def main():
-    save_data = libs.save.SaveData.get_from_json(
-        libs.const.Save.Path.DATA_PATH, libs.const.Save.Path.FILE_NAME
+    save_data = save.SaveData.get_from_json(
+        const.Save.Path.DATA_PATH, const.Save.Path.FILE_NAME
     )
-    libs.gui.create(save_data=save_data, on_start_button_click=_on_start_button_click)
+    gui.create(save_data=save_data, on_start_button_click=_on_start_button_click)
 
 
 if __name__ == "__main__":
