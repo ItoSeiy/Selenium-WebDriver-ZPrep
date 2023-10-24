ba6625ab23b5c388e684459df1530c3bd5d3fca7
ItoSeiya <itoseiya1219@gmail.com>
2023-10-25 02:11:01 +0900

    fix : const test

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -9,7 +9,14 @@ def _on_start_button_click(save_data: save.SaveData):
         save_data.save_to_json(
             save_data, const.Save.Path.DATA_PATH, const.Save.Path.FILE_NAME
         )
-    print(save_data.student_id)
+    print(f"student_id: {save_data.student_id}")
+    print(f"password: {save_data.password}")
+    print(f"chapter_url: {save_data.chapter_url}")
+    print(f"time_out: {save_data.time_out}")
+    print(f"use_sound_notice: {save_data.use_sound_notice}")
+    print(f"use_window_notice: {save_data.use_window_notice}")
+    print(f"mute_video: {save_data.mute_video}")
+    print(f"notice_sound_scale: {save_data.notice_sound_scale}")
 
 
 if __name__ == "__main__":
